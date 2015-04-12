ef117bd73f80d9e533a33b2d83885e4c35454294
dcole77 <dcole@fullsail.edu>
2015-04-12 11:07:25 -0400

    Added Casting
    Added casting a string as a number and a number as a string.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,13 +1,14 @@
-// Expressions - Concatenating Strings
+// Casting
 
-var firstName = "Kermit";
-var lastName = "The Frog";
-var fullName= firstName + " " + lastName;
+var stingVar = "6";
+var result = 7 + Number(stingVar);
+                    //put Number in front of the string number in cased in parenthesis to cast(treat) it as a number
+//console.log(result);
 
-//console.log(fullName)
-
-var a = "6" //doesn't represent numbers, it's a sting due to quotation marks
-var b = "7"
-
-var result = a + b;
-console.log(result)
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+//(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
+                    //put Sting in front of variable in cased in parenthesis to cast it as a string
+console.log(phoneNo);
