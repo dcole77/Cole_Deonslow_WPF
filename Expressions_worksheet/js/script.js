15a1a76eb6aabab2c718fdee94a070b3b93e5468
dcole77 <dcole@fullsail.edu>
2015-04-12 12:45:42 -0400

    Added Expressions Worksheet
    Added expressions worksheet with various problems.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+// Deonslow Cole 4/12/15 Expressions Worksheet.*/
+
+//Dog Years
+
+var humYears = 1; //declare the years of a human
+var dogYears = humYears * 7; //declare dog years, which is human years multiplied by 7
+console.log("Sparky is " + humYears + " human years old which is " + dogYears + " in dog years");
+
