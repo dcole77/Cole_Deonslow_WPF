49c859fbaf5355a586e7217183d634317365ea1c
dcole77 <dcole@fullsail.edu>
2015-04-12 23:27:05 -0400

    Added Slice of Pie part 1
    Added word problem to find the slice of pizzas party goers ate.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,7 +2,24 @@
 
 //Dog Years
 
-var humYears = 1; //declare the years of a human
-var dogYears = humYears * 7; //declare dog years, which is human years multiplied by 7
-console.log("Sparky is " + humYears + " human years old which is " + dogYears + " in dog years");
+//declare the years of a human
+var humYears = 1;
+//declare dog years, which is human years multiplied by 7
+var dogYears = humYears * 7;
+//print out the output of how old Sparky is in relation to human years
+console.log("Sparky is " + humYears + " human years old which is " + dogYears + " in dog years.");
+
+
+//Slice of Pie part 1
+
+//declare slices of pizza
+var pizSlices = 11;
+//declare number of people at the party
+var partyPeople = 32;
+//declare the number of pizzas ordered
+var pizPies = 14;
+//find the average number of slices ate by multiplying the number of slices on a pizza by the number of pizzas and divide by the number of people at the party.
+var eatPizza = pizPies * pizSlices / partyPeople;
+//print out the output of the number of slices each person at the party got
+console.log("Each person ate" + " " + eatPizza + " " + "slices of pizza at the party.");
 
