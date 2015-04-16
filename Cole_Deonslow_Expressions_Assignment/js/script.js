551126b92bf73c8c89ce5e094691df1e0b806408
dcole77 <dcole@fullsail.edu>
2015-04-15 21:11:50 -0400

    User activity input
    Asked user to input the amount of hours they will spend on a activity.

--- Cole_Deonslow_Expressions_Assignment/js/script.js
+++ Cole_Deonslow_Expressions_Assignment/js/script.js
@@ -1,8 +1,20 @@
 /*Deonslow Cole 4/14/15 Expressions Assignment*/
 
 //invite user to to enter their name.
-var name = prompt ("Let's see how many calories you burn! \nPlease enter your name.");
+var name = prompt ("Let's see how many calories you can burn! \nPlease enter your name.");
+
 //use alert to say Hi to the user
 alert("Hi " + name);
+
 //ask the user to enter their weight.
-var weight = prompt(name + ", please enter your weight");
+var weight = prompt(name + ", please enter your weight.");
+
+//invite user to enter the time spent on activities
+alert("Alright "+name+". Let's get started, please enter the hours you plan to spend on each activity in a week.")
+
+//ask user to enter the amount of time they spend on an activity.
+var yogaTime = prompt("How many hours will you spend in yoga? \nUse .5 for half; ex. 1.5");
+var bikingTime = prompt("How many hours will you spend biking? \nUse .5 for half; ex. 1.5");
+var hikeTime = prompt("How many hours will you spend hiking? \nUse .5 for half; ex. 1.5");
+var walkTime = prompt("How many hours will you spend walking? \nUse .5 for half; ex. 1.5");
+var runTime = prompt("How many hours will you spend jogging? \nUse .5 for half; ex. 1.5");
