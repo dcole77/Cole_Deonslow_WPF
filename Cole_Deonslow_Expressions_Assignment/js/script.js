3c6fa0e06ea2df6052b4aa7e5a33245cb6d389a6
dcole77 <dcole@fullsail.edu>
2015-04-15 23:17:14 -0400

    Add calculation of total calories burned
    Calculate the total number of MET values  and the total calories
    burned. Alert user of number of calories burned in a week.

--- Cole_Deonslow_Expressions_Assignment/js/script.js
+++ Cole_Deonslow_Expressions_Assignment/js/script.js
@@ -19,22 +19,36 @@ alert("Alright " + name + ". Let's get started, please enter the hours you plan 
 //ask user to enter the amount of time they spend on an activity.
 var yogaTime = prompt("How many hours will you spend in yoga? \nUse .5 for half; ex. 1.5");
 var bikingTime = prompt("How many hours will you spend biking? \nUse .5 for half; ex. 1.5");
-var hikeTime = prompt("How many hours will you spend hiking? \nUse .5 for half; ex. 1.5");
 var walkTime = prompt("How many hours will you spend walking? \nUse .5 for half; ex. 1.5");
 var runTime = prompt("How many hours will you spend jogging? \nUse .5 for half; ex. 1.5");
 
 //calculate the total time for the week on activities
-var totTime = Number(yogaTime) + Number(bikingTime) + Number(hikeTime) + Number(walkTime) + Number(runTime)
+var totTime = Number(yogaTime) + Number(bikingTime) + Number(walkTime) + Number(runTime)
 console.log(totTime);
 
 //let the user know the total time they spend on weekly activities
 alert(name + ", You plan on spending about " + totTime + " hours a week on these activities.");
 
 //declare Metabolic Equivalent (MET) Values for activities
-var metValues = ["yoga", "biking", "hiking", "walking", "running"]
+var metValues = ["yoga", "biking", "walking", "running"];
 metValues[0] = 3;
 metValues[1] = 8.6;
-metValues[2] = 6;
-metValues[3] = 3.8;
-metValues[4] = 7.5;
+metValues[2] = 3.8;
+metValues[3] = 7.5;
+
+//alert user to see how many calories will be burned
+alert(name + " let's see how many calories you will burn.");
+
+//calculate the total MET values
+var totMet = metValues[0] + metValues[1] + metValues[2] + metValues[3];
+console.log(totMet);
+
+//calculate the Calories burned
+var calBurned = (totMet * kgWeight) * totTime;
+console.log(calBurned);
+
+//alert user of the amount of calories burned in a week
+alert("After " + totTime + " hours of activity " + name+", you would have burned about " + calBurned + " calories in a week.");
+
+
 
