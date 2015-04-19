857ec5079762208c3513d394d5dc81a5873f3212
dcole77 <dcole@fullsail.edu>
2015-04-18 22:18:15 -0400

    Added conditional script
    Added conditional script

--- Conditionals/js/conditionals.js
+++ Conditionals/js/conditionals.js
@@ -1,3 +1,10 @@
-/**
- * Created by deoncole on 4/18/15.
- */
+/* Conditionals*/
+
+var oldEnough=false;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if the condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("What comes after")
