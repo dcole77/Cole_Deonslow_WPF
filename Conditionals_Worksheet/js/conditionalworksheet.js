75a1fc169f7dd14c134aaa9eedd95c7cc1794a81
dcole77 <dcole@fullsail.edu>
2015-04-20 00:00:49 -0400

    Added Last Chance for Gas Problem
    Added Last Chance for Gas Problem

--- Conditionals_Worksheet/js/conditionalworksheet.js
+++ Conditionals_Worksheet/js/conditionalworksheet.js
@@ -1,7 +1,7 @@
 /* Deonslow Cole Conditional Worksheet Created 4/19/15 */
 
 //Celsius to Fahrenheit
-var numDegree = prompt("What is the temperature outside?");
+/*var numDegree = prompt("What is the temperature outside?");
 var degree = prompt("Would you like to convert it to Celsius or Fahrenheit? \nPlease enter 'C' for Celsius or 'F' for Fahrenheit");
 if(degree === "C"){
         degree = (numDegree - 32) * (5/9);
@@ -11,6 +11,15 @@ if(degree === "C"){
 if(degree === "F"){
         degree = (numDegree * 1.8) + 32;
     console.log("The temperature is " + degree + " degrees Fahrenheit.")
-    }
+    }*/
 
+//Last Chance for Gas
 
+var gasEfficiency = 30;
+var gasTank = .75;
+var gasCapacity = 20;
+if (gasCapacity * gasTank * gasEfficiency > 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}else {
+    console.log("You only have " + (gasCapacity * gasTank) + " gallons of gas in your tank, better get gas now while you can!");
+}
