359c64bf9230384085e60f35a3dde7c726a87e10
dcole77 <dcole@fullsail.edu>
2015-04-20 00:50:38 -0400

    Added Check the Login Problem
    Added Check the Login Problem

--- Conditionals_Worksheet/js/conditionalworksheet.js
+++ Conditionals_Worksheet/js/conditionalworksheet.js
@@ -15,11 +15,24 @@ if(degree === "F"){
 
 //Last Chance for Gas
 
-var gasEfficiency = 30;
+/*var gasEfficiency = 30;
 var gasTank = .75;
 var gasCapacity = 20;
 if (gasCapacity * gasTank * gasEfficiency > 200){
     console.log("Yes, you can make it without stopping for gas!");
 }else {
     console.log("You only have " + (gasCapacity * gasTank) + " gallons of gas in your tank, better get gas now while you can!");
+}*/
+
+//Check the Login
+var userName = prompt("Please enter your User Name");
+var userPass = prompt("Please enter your Password");
+var corName = "FullSail15";
+var corPass = "Welcome1";
+if(userName === corName && userPass === corPass){
+    console.log("Welcome, " + userName + "!");
+}else if (userName != corName){
+    console.log("User not found!");
+}else {
+    console.log("Password does not match our records.")
 }
