1ab4c4b2d3012f99c909038b2f7fa7f20ff31e93
dcole77 <dcole@fullsail.edu>
2015-04-22 23:12:46 -0400

    Verify camper's info
    Verify camper's info

--- Cole_Deonslow_Conditionals_Assignment/js/conditionalsassignment.js
+++ Cole_Deonslow_Conditionals_Assignment/js/conditionalsassignment.js
@@ -1,18 +1,23 @@
 /* Deonslow Cole 4/22/15 Conditionals Assignment*/
 
-//ask user to enter their name & participants name in order to begin the camp registration & welcome them.
+//ask user to enter their name & participants name in order to begin the camp registration, verify they are a parent or guardian & welcome them.
 var name = prompt("To begin registering for our sports camp please enter your full name.");
 if (name === ""){
     name = prompt("Ooops, you forgot something, \nPlease enter your full name in order to begin");
 }
-alert("Welcome " + name);
 
 //ask user to enter the participants name
 var camperName = prompt("Please enter the camper's full name.");
 if (camperName === ""){
     camperName = prompt("Please enter camper's full name.")
 }
+//ask user to verify that they are the parent or guardian
+var guardian = prompt("Are you " + camperName + "'s parent or guardian?");
+if (guardian === "yes"){
+    alert("Thank you " + name + ", please continue.")
+}else{alert("Only " + camperName + "'s parent or guardian can fill out this information. \nPlease stop");
 
+}
 //ask user ot input information about the camper; Age, Grade & Weight
 var camperAge = prompt("How old is " + camperName);
 if (camperAge === "") {
@@ -27,3 +32,6 @@ if (camperWeight === "") {
     camperWeight = prompt("Please enter" + camperName + " weight.");
 
 }
+
+//ask user to input
+var section = prompt("What section do you want ");
