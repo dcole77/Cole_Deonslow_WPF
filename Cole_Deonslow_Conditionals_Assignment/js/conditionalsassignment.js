9950cc4a0140411d5e2f437159262a359b2c2257
dcole77 <dcole@fullsail.edu>
2015-04-23 00:51:47 -0400

    Prompt user to input camper's age, grade and weight
    Prompt user to input camper's age, grade and weight

--- Cole_Deonslow_Conditionals_Assignment/js/conditionalsassignment.js
+++ Cole_Deonslow_Conditionals_Assignment/js/conditionalsassignment.js
@@ -4,12 +4,14 @@
 var name = prompt("To begin registering for our sports camp please enter your full name.");
 if (name === ""){
     name = prompt("Ooops, you forgot something, \nPlease enter your full name in order to begin");
+    console.log(name)
 }
 
 //ask user to enter the participants name
 var camperName = prompt("Please enter the camper's full name.");
 if (camperName === ""){
-    camperName = prompt("Please enter camper's full name.")
+    camperName = prompt("Please enter camper's full name.");
+    console.log(camperName)
 }
 //ask user to verify that they are the parent or guardian
 var guardian = prompt("Are you " + camperName + "'s parent or guardian?");
@@ -18,20 +20,27 @@ if (guardian === "yes"){
 }else{alert("Only " + camperName + "'s parent or guardian can fill out this information. \nPlease stop");
 
 }
-//ask user ot input information about the camper; Age, Grade & Weight
+//ask user to input information about the camper; Age, Grade & Weight
 var camperAge = prompt("How old is " + camperName);
 if (camperAge === "") {
     camperAge = prompt("Please enter" + camperName + "'s age.");
+    console.log(camperAge)
 }
 var camperGrade = prompt("What grade is " + camperName + " in?");
 if (camperGrade === "") {
     camperGrade = prompt("Please enter " + camperName + "'s grade.");
+    console.log(camperGrade)
 }
-var camperWeight = prompt("How much does " + camperName + "'s weigh?");
+var camperWeight = prompt("How much does " + camperName + " weigh?");
 if (camperWeight === "") {
-    camperWeight = prompt("Please enter" + camperName + " weight.");
+    camperWeight = prompt("Please enter " + camperName + " weight.");
+    console.log(camperWeight)
 
 }
-
-//ask user to input
-var section = prompt("What section do you want ");
+//ask user to select the group they would like to participate in
+var group = prompt("Which group would you like to join? \nBeginner \nIntermediate \nAdvanced");
+if (group === "Beginner"){
+    (camperAge >= 10 && camperAge <= 12);
+    alert(camperName + " meets the age requirements to be in the Beginner group.");
+}else (camperAge < 10)
+    alert(camperName + "is " + 10-camperAge + " year's too young to bein this group.");
