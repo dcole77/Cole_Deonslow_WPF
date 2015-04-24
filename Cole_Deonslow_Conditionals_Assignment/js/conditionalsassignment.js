36537469dc26fd04a694fc397d4b62cfaefefb5e
dcole77 <dcole@fullsail.edu>
2015-04-23 23:43:00 -0400

    Declared variables for final results
    Declared variables for final results

--- Cole_Deonslow_Conditionals_Assignment/js/conditionalsassignment.js
+++ Cole_Deonslow_Conditionals_Assignment/js/conditionalsassignment.js
@@ -25,10 +25,10 @@ if (gender === "Male"){
 }
 console.log(gender);
 
-//Delcare array of young & older males & females daily number of ounces to drink. Young males and females index 0 & 2. Older males & females index 1 & 3
+//Declare array of young & older males & females daily number of ounces to drink. Young males and females index 0 & 2. Older males & females index 1 & 3
 var dailyOunces = [96.5, 75, 125, 91];
 
-//Delcare an array of male or female.
+//Declare an array of male or female.
 var gender = ["Male", "Female"];
 
 //prompt user to enter the number of cups they drink
@@ -38,14 +38,39 @@ console.log(name + ", you are drinking about " + cupsToDrink * 8 + " ounces a da
 
 //alert user of the amount they should be drinking
 if (gender===[0] && (drinkerAge>8 && drinkerAge<=18)) {
-    alert("You should be drinking" + dailyOunces[0] / 8 + " cups a day.");
-}else if (gender===[0] && (drinkerAge>9 && drinkerAge>=19)){
-    alert("You should be drinking" + dailyOunces[2] / 8 + " cups a day.");
-}else if (gender===[1] && (drinkerAge>8 && drinkerAge <= 18)){
-    alert("You should be drinking" + dailyOunces[1] / 8 + " cups a day.");
-} else (gender===[1] && (drinkerAge>9 && drinkerAge>=19))
-    alert("You should be drinking" + dailyOunces[3] / 8 + " cups a day.");
-console.log("You should be drinking" + dailyOunces[0] / 8 + " cups a day.");
-console.log("You should be drinking" + dailyOunces[2] / 8 + " cups a day.");
-console.log("You should be drinking" + dailyOunces[1] / 8 + " cups a day.");
-console.log("You should be drinking" + dailyOunces[3] / 8 + " cups a day.");
+    alert("You should be drinking " + dailyOunces[0] / 8 + " cups a day.");
+}else if (gender===[0] && (drinkerAge>=19)){
+    alert("You should be drinking " + dailyOunces[2] / 8 + " cups a day.");
+}else if (gender===[1] && (drinkerAge>8 && drinkerAge<=18)){
+    alert("You should be drinking " + dailyOunces[1] / 8 + " cups a day.");
+} else (gender===[1] && (drinkerAge>=19))
+    alert("You should be drinking " + dailyOunces[3] / 8 + " cups a day.");
+console.log("You should be drinking " + dailyOunces[0] / 8 + " cups a day.");
+console.log("You should be drinking " + dailyOunces[2] / 8 + " cups a day.");
+console.log("You should be drinking " + dailyOunces[1] / 8 + " cups a day.");
+console.log("You should be drinking " + dailyOunces[3] / 8 + " cups a day.");
+
+//alert the user to see how much they need to meet the average daily allowance.
+alert("Let's see if you are drinking the daily average.");
+
+//declare an average for ounces to compare
+var maleAverageOunces = ((dailyOunces[0] + dailyOunces[2]) / 2);
+var femaleAverageOunces = ((dailyOunces[1] + dailyOunces[3]) / 2);
+var maleOuncesLess = 125 - maleAverageOunces;
+var femaleOuncesLess = 166 - femaleAverageOunces;
+console.log(maleAverageOunces);
+console.log(femaleOuncesLess);
+
+//Declare the variable for the male daily average and results
+var maleDailyAverage = (maleAverageOunces - cupsToDrink * 8);
+var finalMaleResults;
+finalMaleResults = (maleDailyAverage < 125) ? alert("If you are a male you are drinking " + maleOuncesLess + " ounces less than the recommended daily average") : alert("You are drinking more than the recommended average.");
+
+//Declare the variable for the female daily average and results
+var femaleDailyAverage = (femaleAverageOunces - cupsToDrink * 8);
+var finalFemaleResults;
+finalFemaleResults = (femaleDailyAverage < 166) ? alert("You are a female you are drinking " + femaleOuncesLess + " ounces less than the recommended daily average") : alert("You are drinking more than the recommended average.");
+
+
+
+
