f814516102971e51f6ca0a4da13a305a2effa2ba
dcole77 <dcole@fullsail.edu>
2015-04-26 15:17:58 -0400

    Added Function Execution
    Added Function Execution on how to call a function

--- Functions/js/functions.js
+++ Functions/js/functions.js
@@ -1,12 +1,16 @@
-// Functions - Basic Structure
+// Functions - Function Execution
 
+//basic structure of the function.
 function outputMsg(){
     console.log("Hello World");
 }
 
+//function structure with the code it is to run. This will not write to console.
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
+//To call the function write the function name followed by two parenthesis.
+calcArea();
