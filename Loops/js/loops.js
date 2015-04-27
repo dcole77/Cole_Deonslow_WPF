970bb3b4472743cbb343c21e120fce4bd357b0af
dcole77 <dcole@fullsail.edu>
2015-04-26 21:07:03 -0400

    Added Do While Loop
    Added Do While Loop

--- Loops/js/loops.js
+++ Loops/js/loops.js
@@ -1,5 +1,5 @@
 //Loops
-
+/*
 //While Loops
 var b = 10; //sets up the index
 
@@ -7,3 +7,16 @@ while (b > 0){ //checks the condition
     console.log(b + " kegs on the wall.");
     b--; //increments or decrements the index
 }
+
+
+//Do While Loop
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall.");
+    c--;
+}
+while(c > 0);
+
+
+//For Loop
