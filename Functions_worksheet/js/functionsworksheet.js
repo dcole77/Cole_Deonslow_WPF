b56abb5fca6b5e63ccba50727a7cf29641706c07
dcole77 <dcole@fullsail.edu>
2015-04-27 22:40:17 -0400

    Console the result of the amount of Bee Stings
    Console the result of the amount of Bee Stings

--- Functions_worksheet/js/functionsworksheet.js
+++ Functions_worksheet/js/functionsworksheet.js
@@ -13,4 +13,10 @@ console.log("The circumference of the circle is " + totalRadius);
 
 //Stung Problem
 //declare variable for argument
-var totalWeight = vicWeight (135);
+var totalWeight = vicWeight (125);
+//declare function for bee sting
+function vicWeight (wt){
+    var stings = 8.666666667 * wt;
+    return stings;
+}
+console.log("It take " + totalWeight + " bee stings to kill this animal.")
