3680d5fdf50c38d68115711e65f7f485c3c10585
dcole77 <dcole@fullsail.edu>
2015-04-27 22:10:02 -0400

    Declare the function
    Declare the function

--- Functions_worksheet/js/functionsworksheet.js
+++ Functions_worksheet/js/functionsworksheet.js
@@ -2,4 +2,9 @@
 
 //Circumference
 //declare a variable to hold the arguments
-var totalRadius = ();
+var totalRadius = (6);
+//declare the function
+function calCircumference (r){
+    var circumference = 2 * 3.14 * r;
+    return circumference
+}
