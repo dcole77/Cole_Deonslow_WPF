9f2d916e8b7f82c9bf439cc02209aa831a9230db
dcole77 <dcole@fullsail.edu>
2015-04-28 20:54:50 -0400

    Welcome user and prompt to enter a created name
    Welcome user and prompt to enter a created name

--- Cole_Deonslow_Functions_Assignment/js/functionsAssignment.js
+++ Cole_Deonslow_Functions_Assignment/js/functionsAssignment.js
@@ -1,1 +1,9 @@
 //Deonslow Cole, 4-27-15, Functions Assignment
+
+//welcome user
+alert("Welcome to the Marvel Champion Game");
+//prompt user to enter 
+var champName = prompt("To begin, enter your created Super Hero Champion Name");
+while (champName === ""){
+    champName = prompt("To begin you must create a Super Hero Champion Name. \nWhat will your Hero Name Be?");
+}
