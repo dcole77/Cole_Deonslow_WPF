ddfd0fe24eefacc0fe66ee55db808849237d35e3
dcole77 <dcole@fullsail.edu>
2015-04-30 02:30:41 -0400

    completed functions assignment
    completed functions assignment with tested values.

--- Cole_Deonslow_Functions_Assignment/js/functionsAssignment.js
+++ Cole_Deonslow_Functions_Assignment/js/functionsAssignment.js
@@ -38,7 +38,7 @@ while (weapon2 === "") {
         weapon2 = prompt(champName + " Another weapon will greatly increase your chance of survival! \nChoose your second weapon \n1. Element Gun \n2. Infinity Sword")
     }
     if (weapon2 === "element gun") {
-        weapon2 = Number(skillLevel) + 90
+        weapon2 = Number(skillLevel) + 80
     } else (weapon2 = skillLevel * .70);
 console.log("You've received an additional " + weapon2 + " to your skill level with the addition of your second weapon.");
 var weapon3 = prompt(champName + " choose your third weapon. \n1. Armored Suit  \n2. Proton Cannon");
@@ -47,7 +47,7 @@ while (weapon3 === "") {
         weapon3 = prompt(champName + " You need something to help fight Ultron! \nChoose your third weapon for battle! \n1. Armored Suit  \n2. Proton Cannon");
     }
     if (weapon3 === "armored suit") {
-        weapon3 = Number(skillLevel) + 90
+        weapon3 = Number(skillLevel) + 85
     } else (weapon3 = Number(skillLevel) + 110);
 console.log("Your last weapon has increased your skill level by " + weapon3 + ".");
 //set up function for total skill level
@@ -58,7 +58,7 @@ function completeSkillLevel (w1, w2, w3){
 }
 console.log("Your total skill level is " + totSkillLevel + ".");
 //prompt user to ask if they are ready to battle & validate
-var battleReady = prompt("OH NO! " + champName + ", Utron has arrived sooner than expected, now that you're equipped you'll have to face him. \nAre you ready to fight Ultron? \nYes or No");
+var battleReady = prompt("OH NO! " + champName + ", Utron has arrived sooner than expected, now that you're equipped you'll have to face him alone. \nAre you ready to fight Ultron? \nYes or No");
 battleReady = battleReady.toLowerCase();
 while (battleReady === "" && (battleReady != "yes" || battleReady != "no")) {
     battleReady = prompt(champName + " You must destroy Ultron please type Yes!");
@@ -73,10 +73,10 @@ alert("The crowd is cheering " + champName + "!..." + champName + ", as you rush
 var outcome;
 if (totSkillLevel > villain){
     alert(champName + ", your " + totSkillLevel + " total skill level is enough to destroy Ultron. \nYou've saved the world.")
-}else outcome = prompt (champName + ", your " + totSkillLevel + " total skill level isn't enough to defeat Ultron. \nIron Man has arrived, would you like his help? \nType yes or no");
-var teamUp = outcome;
-(outcome === "yes") ? alert("You and Iron Man are ready to take on Ultron") : alert(champName + ", you have let the world down we are doomed!");
-console.log(teamUp + " you and Iron Man are ready to team up and fight Ultron");
+}else if (outcome = prompt (champName + ", your " + totSkillLevel + " total skill level isn't enough to defeat Ultron. \nIron Man has arrived, would you like his help? \nType yes or no")){
+var teamUp = (outcome === "yes") ? alert("You and Iron Man are ready to take on Ultron") : alert(champName + ", you have let the world down we are doomed!");
+}
+console.log(champName + " you and Iron Man are ready to team up and fight Ultron");
 //declare Iron Man variable
 var ironMan  = 421;
 //declare anonymous function for team up
@@ -87,7 +87,9 @@ var finalTeam = function(ts, im){
 var finBattle = finalTeam(totSkillLevel, ironMan);
 console.log(finBattle);
 //write if statement to compare the team up and the villain
-if (finBattle > villain && teamUp === "yes"){
+if (finBattle > villain && outcome === "yes"){
     alert(champName + ", you and Iron Man have done it!! \nYour combined level of " + finBattle + " was enough to destroy Ultron. The world is safe because of you!! \nThank you " + champName + ".");
 }
 console.log(champName + ", you and Iron Man have done it!! \nYour combined level of " + finBattle + " was enough to destroy Ultron. The world is safe because of you!! \nThank you " + champName + ".");
+
+//I tested the calculations by inputting different numbers for the skill level starting with the lowest 100 which provided a total skill level of 405 when added together with all of the weapons. The total skill level wasn't enough and when added with the value of Iron Man the skill level became 826 which was enough to stop the villian. I tested it again by putting in the highest skill level of 300 which returned an total skill level of 1546, enough to stop the villian.
