cceabce4f61263423672a41acd530ff5f372dd75
dcole77 <dcole@fullsail.edu>
2015-04-29 22:53:49 -0400

    Set up function for total skill level
    Set up function for total skill level

--- Cole_Deonslow_Functions_Assignment/js/functionsAssignment.js
+++ Cole_Deonslow_Functions_Assignment/js/functionsAssignment.js
@@ -3,7 +3,7 @@
 //welcome user
 alert("Welcome to the Marvel Hero's Game");
 //prompt user to enter & validate
-var champName = prompt("To begin, enter your created Super Hero Champion Name");
+var champName = prompt("To begin, create yourself a Super Hero Champion Name");
 while (champName === ""){
     champName = prompt("To begin you must create a Super Hero Champion Name. \nWhat will your Hero Name Be?");
 }
@@ -21,32 +21,39 @@ while(skillLevel === ""){
 }
 console.log("Your skill level is " + skillLevel + ".");
 //declare villain level
-    var villain = 875;
+    var villain = 700;
 //prompt user to pick weapons & validate
-var weapon1 = prompt(champName + "Choose you first weapon. \n1. Power Boost \n2. Partial Defense Boost");
+var weapon1 = prompt(champName + " choose in your first weapon. \n1. Power Boost \n2. Partial Defense Boost");
     weapon1 = weapon1.toLowerCase();
 while (weapon1 === "") {
         weapon1 = prompt(champName + " You can't fight without a weapon! \nChoose your first weapon \n1. Power Boost \n2. Partial Defense Boost")
     }
     if (weapon1 === "power boost") {
         weapon1 = skillLevel * .5
-    } else (weapon1 = skillLevel * .10);
+    } else (weapon1 = skillLevel * .20);
 console.log("You received a boost of " + weapon1 + ".");
-var weapon2 = prompt(champName + "Choose you second weapon. \n1. Element Gun \n2. Infinity Sword");
+var weapon2 = prompt(champName + " choose your second weapon. \n1. Element Gun \n2. Infinity Sword");
 weapon2 = weapon2.toLowerCase();
 while (weapon2 === "") {
         weapon2 = prompt(champName + " Another weapon will greatly increase your chance of survival! \nChoose your second weapon \n1. Element Gun \n2. Infinity Sword")
     }
     if (weapon2 === "element gun") {
-        weapon2 = Number(skillLevel) + 120
-    } else (weapon2 = skillLevel * .45);
+        weapon2 = Number(skillLevel) + 150
+    } else (weapon2 = skillLevel * .70);
 console.log("You've received an additional " + weapon2 + " to your skill level with the addition of your second weapon.");
-var weapon3 = prompt(champName + "Choose you third weapon. \n1. Armored Suit  \n2. Proton Cannon");
+var weapon3 = prompt(champName + " choose your third weapon. \n1. Armored Suit  \n2. Proton Cannon");
 weapon3 = weapon3.toLowerCase();
 while (weapon3 === "") {
         weapon3 = prompt(champName + " You need something to help fight Ultron! \nChoose your third weapon for battle! \n1. Armored Suit  \n2. Proton Cannon");
     }
     if (weapon3 === "armored suit") {
         weapon3 = Number(skillLevel) + 100
-    } else (weapon3 = Number(skillLevel) + 90);
+    } else (weapon3 = Number(skillLevel) + 150);
 console.log("Your last weapon has increased your skill level by " + weapon3 + ".");
+//set up function for total skill level
+var totSkillLevel = completeSkillLevel(weapon1, weapon2, weapon3);
+function completeSkillLevel (w1, w2, w3){
+    var total = (w1 + w2) + w3;
+    return total
+}
+console.log("Your total skill level is " + totSkillLevel + ".");
